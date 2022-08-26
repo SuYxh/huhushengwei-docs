b312a3fc126035d1d5a92e43685a57b0654eb356
yangxinhao <yangxinhao@zhuanzhuan.com>
2022-08-26 09:58:40 +0800

    新增网络内容

--- docs/.vitepress/configs/sidebar/getNetWorkSidebar.js
+++ docs/.vitepress/configs/sidebar/getNetWorkSidebar.js
@@ -15,6 +15,18 @@ module.exports = function getNetWorkSidebar() {
         {
           text: '1.TCP三次握手.md',
           link: '/network/tcp/1.TCP三次握手'
+        },
+        {
+          text: '2.TCP 四次挥手',
+          link: '/network/tcp/2.TCP 四次挥手'
+        },
+        {
+          text: '3.TCP:IP-如何保证数据包传输的有序可靠？',
+          link: '/network/tcp/3.TCP:IP-如何保证数据包传输的有序可靠？'
+        },
+        {
+          text: '4.TCP和UDP的区别',
+          link: '/network/tcp/4.TCP和UDP的区别'
         }
       ]
     },
