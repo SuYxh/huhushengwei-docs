ed6c1f03e46644f74748e97bc485dc156ca0fd4d
yangxinhao <yangxinhao@zhuanzhuan.com>
2022-08-28 11:36:59 +0800

    feat: 工程化

--- docs/.vitepress/configs/sidebar/getWorkflowSidebar.js
+++ docs/.vitepress/configs/sidebar/getWorkflowSidebar.js
@@ -1,6 +1,15 @@
 module.exports = function getWorkflowSidebar() {
   return [
     {
+      text: 'Babel',
+      children: [
+        {
+          text: '1.简单描述一下 Babel 的编译过程',
+          link: '/workflow/babel/1.简单描述一下 Babel 的编译过程'
+        }
+      ]
+    },
+    {
       text: 'Vite',
       children: [
         {
@@ -13,8 +22,8 @@ module.exports = function getWorkflowSidebar() {
       text: 'webpack',
       children: [
         {
-          text: '01-你对webpack的理解',
-          link: '/workflow/webpack/01-你对webpack的理解'
+          text: 'webpack优化',
+          link: '/workflow/webpack/1.webpack 做过哪些优化，开发效率方面、打包策略方面等等'
         }
       ]
     },
