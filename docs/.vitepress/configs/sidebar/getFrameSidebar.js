170de2f016dff3034a944c1b1509b8ae31f241ed
yangxinhao <yangxinhao@zhuanzhuan.com>
2022-08-28 11:25:40 +0800

    feat: 新增react题目

--- docs/.vitepress/configs/sidebar/getFrameSidebar.js
+++ docs/.vitepress/configs/sidebar/getFrameSidebar.js
@@ -13,8 +13,36 @@ module.exports = function getFrameSidebar() {
       text: 'React',
       children: [
         {
-          text: '01-react的响应式实现',
-          link: '/frame/react/01-react的响应式实现'
+          text: '1.hooks用过吗？聊聊react中class组件和函数组件的区别',
+          link: '/frame/react/1.hooks用过吗？聊聊react中class组件和函数组件的区别'
+        },
+        {
+          text: '2.React 组件通信方式',
+          link: '/frame/react/2.React 组件通信方式'
+        },
+        {
+          text: '3.setState 既存在异步情况也存在同步情况',
+          link: '/frame/react/3.setState 既存在异步情况也存在同步情况'
+        },
+        {
+          text: '4.生命周期',
+          link: '/frame/react/4.生命周期'
+        },
+        {
+          text: '5.说一下 react-fiber',
+          link: '/frame/react/5.说一下 react-fiber'
+        },
+        {
+          text: '6.Portals',
+          link: '/frame/react/6.Portals'
+        },
+        {
+          text: '7.何时要使用异步组件？如和使用异步组件',
+          link: '/frame/react/7.何时要使用异步组件？如和使用异步组件'
+        },
+        {
+          text: '8.React 事件绑定原理',
+          link: '/frame/react/8.React 事件绑定原理'
         }
       ]
     },
