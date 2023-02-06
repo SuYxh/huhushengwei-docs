7d2991641e78c0d9d3cc94a875d0554ae6f08f1b
yangxinhao <yangxinhao@zhuanzhuan.com>
2023-02-06 21:02:42 +0800

    feat: practice

--- playGround.ts
+++ playGround.ts
@@ -1,5 +1,8 @@
-
-
 export function funcPlayGround() {
   console.log('funcPlayGround')
+  const map = new Map<number, string>()
+
+  map.set(2, '2')
+
+  console.log(map)
 }
---
+++ src/html&js/map/index.ts
@@ -0,0 +1,7 @@
+const map = new Map<number, string>()
+
+
+map.set(2, '2')
+
+
+console.log(map)
